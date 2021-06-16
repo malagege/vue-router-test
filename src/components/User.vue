9649555832b6732ed355f765b0047e733294f3a4
malagege <s9714020@gmail.com>
2021-06-16 22:27:26 +0800

    響應路由參數的變化

--- src/components/User.vue
+++ src/components/User.vue
@@ -3,3 +3,22 @@
         User  {{ $route.params.id }}
     </div>
 </template>
+
+<script>
+export default {
+  created() {
+    this.$watch(
+      () => this.$route.params,
+      (toParams, previousParams) => {
+        console.log('toParams',toParams,'previousParams',previousParams);
+      }
+    )
+  },
+//   使用 beforeRouteUpdate 導航守衛，它也可以取消導航
+  async beforeRouteUpdate(to, from) {
+    // 對路由變化做出響應...
+    // this.userData = await fetchUser(to.params.id)
+    console.log('to',to,'from',from);
+  },
+}
+</script>

8d7a44b6059b3caf6ad648056817af4c8ec9060c
malagege <s9714020@gmail.com>
2021-06-16 21:02:05 +0800

    first set router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>Hello App!</h1>
+  <p>
+    <!--使用 router-link 組件進行導航 -->
+    <!--通過傳遞 `to` 來指定鏈接 -->
+    <!--`<router-link>` 將呈現一個帶有正確 `href` 屬性的 `<a>` 標籤-->
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/about">Go to About</router-link>
+  </p>
+  <!-- 路由出口 -->
+  <!-- 路由匹配到的組件將渲染在這裡 -->
+  <router-view></router-view>
 </template>
 
 <script setup>
+import Home from './components/Home.vue'
+import About from './components/About.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
 
-createApp(App).mount('#app')
+// 2. 定義一些路由
+// 每個路由都需要映射到一個組件。
+// 我們後面再討論嵌套路由。
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+  ]
+  
+// 3. 創建路由實例並傳遞 `routes` 配置
+// 你可以在這裡輸入更多的配置，但我們在這裡
+// 暫時保持簡單
+const router = createRouter({
+    // 4. 內部提供了 history 模式的實現。為了簡單起見，我們在這裡使用 hash 模式。
+    history: createWebHashHistory(),
+    routes, // `routes: routes` 的縮寫
+  })
+  
+
+createApp(App).use(router).mount('#app')

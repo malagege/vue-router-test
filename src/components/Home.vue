75bf26ebf64ec5c2c4dc6f5476e86bb517327e65
malagege <s9714020@gmail.com>
2021-06-18 16:25:29 +0800

    $router.push 帶中文參數

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
         Home
+        <button v-on:click="pushLink">中文測試</button>
     </div>
 </template>
+
+<script>
+const username = '中文測試'
+// 我們可以手動建立 url，但我們必須自己處理編碼
+
+export default {
+    methods: {
+        pushLink(){
+            this.$router.push(`/users/${username}`) // -> /user/eduardo
+        }
+    }
+}
+</script>
